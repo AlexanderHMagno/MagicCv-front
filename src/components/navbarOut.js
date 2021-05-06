da046e25ad8707112e92141d48ddcc7348d5ba1c
Alex <alexander.hortua10@gmail.com>
2021-05-06 00:04:10 -0700

    Updating landing Page

--- src/components/navbarOut.js
+++ src/components/navbarOut.js
@@ -22,33 +22,33 @@ const Menubar = () => {
     return (
         
         <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 bg-white  lg:max-w-2xl lg:w-full ">
+        <div className="relative z-10 bg-white lg:w-full ">
 
-        <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-            <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-            <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                <div className="flex items-center justify-between w-full md:w-auto">
-                <a href="/">
-                    <span className="sr-only">Magic Cv</span>
-                    <img className="h-10  sm:h-10" src="/images/cv.png" alt="Logo"/>
-                </a>
-                <div className="-mr-2 flex items-center md:hidden">
-                    <Submenu options={options}/>
+        <div className="w-full relative pt-6 px-4 sm:px-6 lg:px-8  ">
+            <nav className="relative flex items-center justify-between sm:h-10 lg:justify-between" aria-label="Global">
+                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                    <div className="flex items-center justify-between w-full md:w-auto">
+                    <a href="/">
+                        <span className="sr-only">Magic Cv</span>
+                        <img className="h-10  sm:h-10" src="/images/cv.png" alt="Logo"/>
+                    </a>
+                    <div className="-mr-2 flex items-center md:hidden">
+                        <Submenu options={options}/>
+                    </div>
+                    </div>
                 </div>
+                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                    <a href="/" className={currentPath === 'Home' ? active : defaulty}>Product</a>
+
+                    <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Features</a>
+
+
+                    <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Company</a>
+
+                    <a href="/register" className={currentPath === 'register' ? active : defaulty}>Register</a>
+
+                    <a className="inline-block border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-2 md:text-lg md:px-10" href="/login" >Log in</a>
                 </div>
-            </div>
-            <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                <a href="/" className={currentPath === 'Home' ? active : defaulty}>Product</a>
-
-                <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Features</a>
-
-
-                <a href="#" className="font-medium text-gray-500 hover:text-gray-900">Company</a>
-
-                <a href="/register" className={currentPath === 'register' ? active : defaulty}>Register</a>
-
-                <a href="/login" className={currentPath === 'login' ? active : defaulty}>Log in</a>
-            </div>
             </nav>
         </div>
 
--- src/pages/landing/Presentation.js
+++ src/pages/landing/Presentation.js
@@ -5,33 +5,45 @@ const Presentation = () => {
 
     return (
         
-            <div className="overflow-hidden min-h-full flex justify-center items-center">
+            <div className="overflow-hidden min-h-full flex ">
                 <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-                        <div className="sm:text-center lg:text-left">
-                        <h1 className="text-4xl tracking-tight font-extrabold  sm:text-5xl md:text-6xl">
-                            <span className="block xl:inline">Create your unique </span>
-                            <span className="block text-indigo-600 xl:inline">CV</span>
-                        </h1>
-                        <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                            Not worry about the design we have you covered, we have ready to print templates for you
-                        </p>
-                        <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                            <div className="rounded-md shadow">
-                            <a href="/register" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                                Register
-                            </a>
+                        <div className="mx-auto sm:text-center lg:text-left h-full">
+                            <h1 className="text-6xl tracking-tight font-extrabold text-center">
+                                <span className="inline xl:inline">Create your unique </span>
+                                <span className="inline text-indigo-600 xl:inline">CV</span>
+                            </h1>
+                            <p className="mt-3  text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl ">
+                                Not worry about the design we have you covered, we have ready to print templates for you
+                            </p>
+                            <div className="mx-auto mt-5 sm:mt-8 sm:flex sm:justify-center ">
+                                <div className="rounded-md shadow">
+                                <a href="/register" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                                    Register
+                                </a>
+                                </div>
+                                <div className="mt-3 sm:mt-0 sm:ml-3">
+                                <a href="/login" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                                    Login
+                                </a>
+                                </div>
                             </div>
-                            <div className="mt-3 sm:mt-0 sm:ml-3">
-                            <a href="/login" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-                                Login
-                            </a>
+                            <div>
+                                <img className="w-4/5 sm:w-2/3 lg:w-3/5 mx-auto" src="images/presentation/people.png" alt="people working"/>
                             </div>
-                        </div>
                         </div>
                 </main>
                 {/* <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
                     <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://cdn.pixabay.com/photo/2017/05/09/00/15/resume-2296951_1280.png" alt=""/>
                 </div> */}
+
+                <div className="hidden">
+
+                    <span>Attributes</span>
+                    <span>working image: </span>
+                    <a href='https://pngtree.com/so/education-clipart'>education clipart png from pngtree.com</a>
+                    <span>people image: </span>
+                    <a href='https://pngtree.com/so/app'>app png from pngtree.com</a>
+                </div>
             </div>
         
     )
