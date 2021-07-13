5ce4c3b44de113ddfa630207514f88aab1a7a430
Alexander Hortua <alexander@datzsolutions.com>
2021-07-12 20:41:25 -0700

    Updating state when the user adds or remove templ

--- src/pages/ideas/util/toggleTemplate.js
+++ src/pages/ideas/util/toggleTemplate.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React , {useContext, useState} from 'react';
 import { useMutation, gql } from '@apollo/client';
 import Button from '@material-ui/core/Button';
+import {GET_PROFILE, GET_TEMPLATES} from '../../../graphql/queries';
+import {AuthContext} from '../../../context/AuthContext';
+
 
 const ToggleTemplate = ({templateId, userLiked}) => {
 
 
         //We need to get the other ideas, susbcribe and unsucbribe to this ideas
+    const [inQueue, setInqueueu] = useState(userLiked);
+    const {user} = useContext(AuthContext);
+    const [toggleTemplateMutation] = useMutation(TOGGLE_TEMPLATE_MUTATION, {
+        update(cache, result) {
+            
+            const TEMPLATECOLLECTION = cache.readQuery({query:GET_PROFILE, variables : {userId:user.id}});        
+            const newData = {getProfile: {...TEMPLATECOLLECTION.getProfile, ...result.data.toggleTemplate }};
+            cache.writeQuery({query:GET_PROFILE, data:newData, variables: { userId:user.id }});
+            
+            setInqueueu(!inQueue);
+        },
+        onError(err) {
+            console.log(err)
+        },
+        variables : {templateId},
+        refetchQueries : () => [{
+            query:GET_TEMPLATES, variables : {userId:user.id}
+        }]
+    });
 
-    // const [toggleTemplateMutation] = useMutation(TOGGLE_TEMPLATE_MUTATION, {
-    //     update(proxy, newData) {
-    //        console.log(newData);
-    //     },
-    //     onError(err) {
-    //         console.log(err)
-    //         // setErrors(err.graphQLErrors[0].extensions.errors);
-    //     },
-    //     variables : {templateid}
-    // });
-
-    const toggleTemplate = () => {
-        console.log( templateId);
-        // toggleTemplateMutation();
-    }
 
 return (
-    <Button onClick={toggleTemplate} variant="contained" color={userLiked? 'secondary':'default'}>
-        {userLiked ? 'Remove':'Add'}
+    <Button onClick={toggleTemplateMutation} variant="contained" color={inQueue? 'secondary':'default'}>
+        {inQueue ? 'Remove':'Add'}
     </Button> 
 )
 }
@@ -34,13 +41,9 @@ return (
 export default ToggleTemplate;
 
 const TOGGLE_TEMPLATE_MUTATION = gql`
-mutation toggleTemplate
-    (
-        $templateId: ID,
-    )
-    { toggleTemplate (
-        templateId: $templateId
-    )}{
+mutation toggleTemplate($templateId: ID!){ 
+    toggleTemplate (templateId: $templateId) {
         templates
-    }
-`
+     }
+}
+`;
